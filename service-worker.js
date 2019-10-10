3ee4cb0c549f85457f3c7c99e688bbaa13697b20
bui hieu <buihieu@buis-MacBook-Pro.local>
2019-10-10 17:41:00 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/macaw/precache-manifest.7e28519189a1115071c80a7c66376c4f.js"
+  "/macaw/precache-manifest.734cb8f14fa9b073682bbd1351d3743f.js"
 );
 
 self.addEventListener('message', (event) => {
